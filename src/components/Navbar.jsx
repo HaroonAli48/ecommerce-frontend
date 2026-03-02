15c5864dfe4c75d7bdb941b5dd2eeddef2d7022f
HaroonAli48 <haroonaliali.pro@gmail.com>
2026-03-02 19:24:46 +0500

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -248,7 +248,7 @@ const Navbar = () => {
       </nav>
 
       <div
-        className={`fixed top-0 right-0 h-full w-64 bg-white md:hidden shadow-lg transform transition-transform duration-300 ease-in-out z-50 ${
+        className={`fixed top-0 right-0 h-full w-64 bg-white lg:hidden shadow-lg transform transition-transform duration-300 ease-in-out z-50 ${
           mobileMenuOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
